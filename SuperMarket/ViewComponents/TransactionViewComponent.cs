c0f583d53453aa26dab3fb9e82f92f03d653eebe
Khoa <ankhoa.dev@gmail.com>
2024-01-13 23:59:39 +0700

    Cashier's Console 2

---
+++ SuperMarket/ViewComponents/TransactionViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperMarket.Models;
+
+namespace SuperMarket.ViewComponents
+{
+    public class TransactionViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string userName)
+        {
+            var transaction = TransactionRepository.GetByDayAndCashier(userName, DateTime.Now);
+            return View(transaction);
+        }
+    }
+}
