38860843193508984d46ff5e1f92fd5887099518
Khoa <ankhoa.dev@gmail.com>
2024-01-14 11:59:11 +0700

    Search (Transaction Report)

---
+++ SuperMarket/Controllers/TransactionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperMarket.Models;
+using SuperMarket.ViewModel;
+
+namespace SuperMarket.Controllers
+{
+    public class TransactionController : Controller
+    {
+        public IActionResult Index()
+        {
+            TransactionViewModel transactionViewModel = new TransactionViewModel();
+            return View(transactionViewModel);
+        }
+
+        public IActionResult Search(TransactionViewModel transactionViewModel)
+        {
+            var transactions = TransactionRepository.Search(transactionViewModel.CashierName??string.Empty, transactionViewModel.StartDate, transactionViewModel.EndDate);
+            transactionViewModel.Transactions = transactions;
+            return View("Index", transactionViewModel);
+        }
+    }
+}
---
+++ SuperMarket/ViewModel/TransactionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SuperMarket.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace SuperMarket.ViewModel
+{
+    public class TransactionViewModel
+    {
+        [Display(Name = "Cashier Name")]
+        public string? CashierName { get; set; }
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
+        public IEnumerable<Transaction> Transactions { get; set; } = new List<Transaction>();
+    }
+}
