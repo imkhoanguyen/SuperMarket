9a8c837146d795cfd5e55ea0e14b129bf5565e84
Khoa <ankhoa.dev@gmail.com>
2024-01-17 11:01:05 +0700

    SellProduct UseCase

--- UseCases/ProductsUseCases/SellProductUseCase.cs
+++ UseCases/ProductsUseCases/SellProductUseCase.cs
@@ -4,21 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UseCases.DataStorePluginInterfaces;
+using UseCases.TransactionUseCase;
 
 namespace UseCases.ProductsUseCases
 {
     public class SellProductUseCase
     {
         private readonly IProductRepository productRepository;
+        private readonly IRecordTransactionUseCase recordTransactionUseCase;
 
-        public SellProductUseCase(IProductRepository productRepository)
+        public SellProductUseCase(IProductRepository productRepository, IRecordTransactionUseCase recordTransactionUseCase)
         {
             this.productRepository = productRepository;
+            this.recordTransactionUseCase = recordTransactionUseCase;
         }
 
-        public void Execute()
+        public void Execute(string cashierName, int productId, int qtyToSell)
         {
-            productRepository.Sell
+            var product = productRepository.GetById(productId);
+            if (product == null) return;
+
+            recordTransactionUseCase.Execute(cashierName, productId, qtyToSell);
+            product.Quantity -= qtyToSell;
+            productRepository.UpdateProduct(productId, product);
         }
     }
 }
